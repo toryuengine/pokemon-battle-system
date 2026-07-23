9d53fcd38ddd37f8474ec378bdfeec3d8faee1a9
toryuengine <toryuengine@gmail.com>
2026-07-24 00:32:49 +0900

    commit

--- src/tern_cpp.cpp
+++ src/tern_cpp.cpp
@@ -2,8 +2,9 @@
 #include <cstdlib> // std::system のために必要
 #include <string>
 #include <vector>
+
+//ステータス実数値
 struct Status {
-    int lv;
     int hp;
     int attack;
     int defense;
@@ -53,11 +54,193 @@ struct savedata {
     int personality;//性格
 };
 
+class CharaCter {
+    private:
+        savedata data;
+        Status status;
+        Racial racial;//種族値構造体
+
+    //ステータス実数値を計算して、status構造体に値を入れる
+        void setStatus() {
+            //種族値の取得
+            getRacial();
+
+            status.hp = calcStatus(data.lv, racial.hp, data.indivisual.hp, data.effort.hp);
+            status.attack = calcStatus(data.lv, racial.attack, data.indivisual.attack, data.effort.attack);
+            status.defense = calcStatus(data.lv, racial.defense, data.indivisual.defense, data.effort.defense);
+            status.sp_attack = calcStatus(data.lv, racial.sp_attack, data.indivisual.sp_attack, data.effort.sp_attack);
+            status.sp_defence = calcStatus(data.lv, racial.sp_defence, data.indivisual.sp_defence, data.effort.sp_defence);
+            status.speed = calcStatus(data.lv, racial.speed, data.indivisual.speed, data.effort.speed);
+
+            //性格を適用する
+            applyPersonality();
+        };
+
+        
+
+        //種族値をsavedataのidから検索して、構造体に保存する
+        void getRacial() {
+            //検索は後で追加
+            //例）バクフーン
+            racial = {50, 78, 84, 78, 109, 85, 100};
+
+        }
+
+        //ステータスを算出する
+        //レベル, 種族値, 個体値, 努力値
+        int calcStatus(int lv, int rac, int ind, int eff) {
+            int calkEffort = eff / 4;
+            int calk1 = rac * 2 + ind + calkEffort;
+            int calk2 = calk1 * lv;
+            int calk3 = calk2 / 100;
+            int finalStatus = calk3 + 5;
+
+            return finalStatus;
+        };
+
+        //性格補正の適用
+        void applyPersonality() {
+            switch (data.personality){
+            case 1://いじっぱり
+                status.attack = status.attack * 110 / 100;
+                status.sp_attack = status.sp_attack * 90 / 100;
+                break;
+
+            case 2://おくびょう
+                status.speed = status.speed * 110 / 100;
+                status.attack = status.attack * 90 / 100;
+                break;
+
+            case 3://がんばりや
+                break;
+
+            case 4://さみしがり
+                status.attack = status.attack * 110 / 100;
+                status.defense = status.defense * 90 / 100;
+                break;
+
+            case 5://ゆうかん
+                status.attack = status.attack * 110 / 100;
+                status.speed = status.speed * 90 / 100;
+                break;
+
+            case 6://やんちゃ
+                status.attack = status.attack * 110 / 100;
+                status.sp_defence = status.sp_defence * 90 / 100;
+                break;
+
+            case 7://ずぶとい
+                status.defense = status.defense * 110 / 100;
+                status.attack = status.attack * 90 / 100;
+                break;
+
+            case 8://すなお
+                break;
+
+            case 9://のんき
+                status.defense = status.defense * 110 / 100;
+                status.speed = status.speed * 90 / 100;
+                break;
+
+            case 10://わんぱく
+                status.defense = status.defense * 110 / 100;
+                status.sp_attack = status.sp_attack * 90 / 100;
+                break;
+
+            case 11://のうてんき
+                status.defense = status.defense * 110 / 100;
+                status.sp_defence = status.sp_defence * 90 / 100;
+                break;
+
+            case 12://せっかち
+                status.speed = status.speed * 110 / 100;
+                status.defense = status.defense * 90 / 100;
+                break;
+
+            case 13://まじめ
+                break;
+
+            case 14://ようき
+                status.speed = status.speed * 110 / 100;
+                status.sp_attack = status.sp_attack * 90 / 100;
+                break;
+
+            case 15://むじゃき
+                status.speed = status.speed * 110 / 100;
+                status.sp_defence = status.sp_defence * 90 / 100;
+                break;
+
+            case 16://ひかえめ
+                status.sp_attack = status.sp_attack * 110 / 100;
+                status.attack = status.attack * 90 / 100;
+                break;
+
+            case 17://おっとり
+                status.sp_attack = status.sp_attack * 110 / 100;
+                status.defense = status.defense * 90 / 100;
+                break;
+
+            case 18://うっかりや
+                status.sp_attack = status.sp_attack * 110 / 100;
+                status.sp_defence = status.sp_defence * 90 / 100;
+                break;
+
+            case 19://れいせい
+                status.sp_attack = status.sp_attack * 110 / 100;
+                status.speed = status.speed * 90 / 100;
+                break;
+
+            case 20://てれや
+                break;
+
+            case 21://おだやか
+                status.sp_defence = status.sp_defence * 110 / 100;
+                status.attack = status.attack * 90 / 100;
+                break;
+
+            case 22://おとなしい
+                status.sp_defence = status.sp_defence * 110 / 100;
+                status.defense = status.defense * 90 / 100;
+                break;
+
+            case 23://なまいき
+                status.sp_defence = status.sp_defence * 110 / 100;
+                status.speed = status.speed * 90 / 100;
+                break;
+
+            case 24://しんちょう
+                status.sp_defence = status.sp_defence * 110 / 100;
+                status.sp_attack = status.sp_attack * 90 / 100;
+                break;
+
+            case 25://きまぐれ
+                break;
+
+            default:
+                break;
+            };
+
+        };
+
+    public:
+        //setと同時にステータス実数地を計算する
+        void setData(savedata arg) {
+            data = arg;
+            setStatus();
+        };
+
+
+        //ステータスを取り出す
+        Status getStatus() const {
+            return status;
+        };
+};
+
 class Basic_status_calc {
     private:
         Racial Typhlosion = {50, 78, 84, 78, 109, 85, 100};
         Racial Swampert = {50, 100, 110, 90, 85, 90, 60};
-        
+        savedata data;
         Status status;
 
     public:
@@ -92,35 +275,61 @@ class Basic_status_calc {
             return finalStatus;
         };
 
-        Status getStatus() {
-            return status;
+        savedata getStatus() {
+            return data;
+        };
+
+        void setCharacter() {
+            data.id = 00001;
+            data.identifier = 00001;
+            data.lv = 50;
+            data.indivisual = {31, 31, 31, 31, 31, 31};
+            data.effort = {255, 255, 0, 0, 0, 0};
+            data.personality = 1;
         };
 };
 
 
 int main() {
     system("chcp 65001");
-    while (true){
-        std::cout << "バクフーン" ;
-        std::cout << "" ;
-        std::cout << "ラグラージ" ;
-        std::cout << "" ;
-        std::cout << "フシギバナ" ;
-        
-    }
-
     Basic_status_calc basic;
-    Racial me = basic.getPokemon1();
-    basic.setRacial(me);
-    Status status = basic.getStatus();
+    basic.setCharacter();
+    savedata data = basic.getStatus();
+
+    CharaCter character;
+    character.setData(data);
+    Status status = character.getStatus();
     
+    //確認のための出力
     std::cout << status.hp << std::endl;
     std::cout << status.attack << std::endl;
     std::cout << status.defense << std::endl;
     std::cout << status.sp_attack << std::endl;
     std::cout << status.sp_defence << std::endl;
     std::cout << status.speed << std::endl;
+
     return 0;
+    // while (true){
+    //     std::cout << "バクフーン" ;
+    //     std::cout << "" ;
+    //     std::cout << "ラグラージ" ;
+    //     std::cout << "" ;
+    //     std::cout << "フシギバナ" ;
+        
+    // }
+
+    // Basic_status_calc basic;
+    // Racial me = basic.getPokemon1();
+    // basic.setRacial(me);
+    // Status status = basic.getStatus();
+    
+    // std::cout << status.hp << std::endl;
+    // std::cout << status.attack << std::endl;
+    // std::cout << status.defense << std::endl;
+    // std::cout << status.sp_attack << std::endl;
+    // std::cout << status.sp_defence << std::endl;
+    // std::cout << status.speed << std::endl;
+    // return 0;
 
 
     // int i = 1;
