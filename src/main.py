4e320a9f765021ea14fec7942c03bf603d91e27a
toryuengine <toryuengine@gmail.com>
2026-09-08 21:27:13 +0900

    技の選択をランダムになるように修正

--- src/main.py
+++ src/main.py
@@ -1,38 +1,28 @@
+import random
+
 from core.battle import Battle
-from core.type_chart import describe_effectiveness
 from pokemon import Pokemon
 
 
 def main():
-    pokemon1 = Pokemon(1, 2)
-    pokemon2 = Pokemon(3, 0)
+    winrate = {}
 
-    battle = Battle(pokemon1, pokemon2)
+    for i in range(10000):
+        pokemon1 = Pokemon(1, 2)
+        pokemon2 = Pokemon(3, 0)
+        battle = Battle(pokemon1, pokemon2)
 
-    print(f"{pokemon1.name} vs {pokemon2.name}")
+        turn = 0
+        while battle.get_winner() is None:
+            attacker, defender = (pokemon1, pokemon2) if turn % 2 == 0 else (pokemon2, pokemon1)
+            move = random.choice(attacker.moves)
+            battle.use_move(attacker, defender, move)
+            turn += 1
 
-    turn = 0
-    while battle.get_winner() is None:
-        attacker, defender = (pokemon1, pokemon2) if turn % 2 == 0 else (pokemon2, pokemon1)
-        move = attacker.moves[0]
+        winner = battle.get_winner()
+        winrate[winner.name] = winrate.get(winner.name, 0) + 1
 
-        result = battle.use_move(attacker, defender, move)
-
-        if not result["hit"]:
-            print(f"{attacker.name}の{move.name}!  しかし外れた")
-        elif move.category == "変化":
-            print(f"{attacker.name}の{move.name}!")
-        else:
-            print(
-                f"{attacker.name}の{move.name}! {defender.name}に{result['damage']}ダメージ "
-                f"({describe_effectiveness(result['effectiveness'])})"
-            )
-            print(f"  {defender.name}の残りHP: {battle.get_current_hp(defender)}")
-
-        turn += 1
-
-    winner = battle.get_winner()
-    print(f"{winner.name}の勝ち！")
+    print(winrate)
 
 
 if __name__ == "__main__":
