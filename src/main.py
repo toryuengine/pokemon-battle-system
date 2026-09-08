0322ef069af19d93b5ddba7e8e5912c997418c22
toryuengine <toryuengine@gmail.com>
2026-09-08 22:50:53 +0900

    処理がバトルクラスと重複していたため削除

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import random
-
 from core.battle import Battle
 from pokemon import Pokemon
 
@@ -12,12 +10,7 @@ def main():
         pokemon2 = Pokemon(3, 0)
         battle = Battle(pokemon1, pokemon2)
 
-        turn = 0
-        while battle.get_winner() is None:
-            attacker, defender = (pokemon1, pokemon2) if turn % 2 == 0 else (pokemon2, pokemon1)
-            move = random.choice(attacker.moves)
-            battle.use_move(attacker, defender, move)
-            turn += 1
+        battle.start_battle()
 
         winner = battle.get_winner()
         winrate[winner.name] = winrate.get(winner.name, 0) + 1
